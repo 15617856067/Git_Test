f3ff86b4f861a6310ae261fef35c6ee80c511231
fsp1990 <cheyy163@126.com>
2018-08-29 16:08:41 +0800

    点点滴滴

---
+++ HS_Demo/HS_Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HS_Demo
+//
+//  Created by FU JUN on 2018/8/29.
+//  Copyright © 2018年 puyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
