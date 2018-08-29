860b9306c3bf1c6f73c7380201ec6240b7f8bd97
fsp1990 <cheyy163@126.com>
2018-08-29 17:59:21 +0800

    抱抱抱抱抱抱

--- HS_Demo/HS_Demo/AppDelegate.m
+++ HS_Demo/HS_Demo/AppDelegate.m
@@ -17,7 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    NSLog(@"点点滴滴AppDelegate");
     
     NSLog(@"aaaaaaaaaa");
     
